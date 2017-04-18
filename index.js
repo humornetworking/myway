dfa8118ca0e4235026f93d3e13dbe8bee761205e
AVP <holamundo.buz@gmail.com>
2017-04-18 15:50:39 -0300

    lalal

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ MongoClient.connect(db, function (err, db) {
 
     });
 	
-
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
