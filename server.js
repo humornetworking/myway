785f656a88804c8f332c47dd5be830f6817a68dc
Andres <holamundo.buz@gmail.com>
2017-05-02 15:12:13 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -300,6 +300,11 @@ MongoClient.connect(db, function (err, db) {
 	};
 	
 	
-app.listen(3000, function () {
+/* app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
+}); */
+
+app.set('port', (process.env.PORT || 5000));
+ app.listen(app.get('port'), function() {
+   console.log('Node app is running on port', app.get('port'));
+ });
